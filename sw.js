b89103b76eb7617314d5275588714bd4a01cc08b
bigviv <73597575+bigviv@users.noreply.github.com>
2026-01-06 18:20:40 +0530

    Update cache name to 'zerolog-v53-cache'

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = 'zerolog-v2';
+const CACHE_NAME = 'zerolog-v53-cache';
 const ASSETS = [
+  '/zerolog/',
   '/zerolog/index.html',
   '/zerolog/manifest.json',
   '/zerolog/logo512.png',
   '/zerolog/screenshot.png'
 ];
 
+// Install: Standard requirement to save files for offline use
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Fetch: This listener is MANDATORY to pass the PWABuilder performance check
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // Return cached file if found, otherwise fetch from network
       return response || fetch(event.request);
     })
   );
